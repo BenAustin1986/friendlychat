c6b647864eb2165ee8b21757dd31dbadb674668e
BenAustin1986 <bena@monsterrg.com>
2024-10-06 21:47:48 -0400

    adding google auth

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,3 +10,12 @@ export const environment = {
     measurementId: 'G-MEASUREMENT_ID',
   },
 };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQWMhplCiFcoxtA5ygxZS897BlwJCrgkM",
+  authDomain: "friendlychat-77e4d.firebaseapp.com",
+  projectId: "friendlychat-77e4d",
+  storageBucket: "friendlychat-77e4d.appspot.com",
+  messagingSenderId: "196600319209",
+  appId: "1:196600319209:web:b4d47b06d39282adeef478"
+};
